02f6cffc8ea6ff3d75362e12fb840d550be24ac8
Dmitriy <velkov55@gmail.com>
2019-08-29 11:40:05 +0300

    Updates

---
+++ precache-manifest.992316378b752eca5771b7f0e35ae058.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2e0c9f467b4a4c79a5292c19bf1cc5a",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "dd47bc0aafc39a38ca38",
+    "url": "/goit-react-hw-01-components/static/css/main.a9879a6c.chunk.css"
+  },
+  {
+    "revision": "9284f6534d3678f4a9b3",
+    "url": "/goit-react-hw-01-components/static/js/2.7c7e473f.chunk.js"
+  },
+  {
+    "revision": "dd47bc0aafc39a38ca38",
+    "url": "/goit-react-hw-01-components/static/js/main.16b7e863.chunk.js"
+  },
+  {
+    "revision": "3fff2c44a6ade889c1dc",
+    "url": "/goit-react-hw-01-components/static/js/runtime~main.83f0207d.js"
+  }
+]);
